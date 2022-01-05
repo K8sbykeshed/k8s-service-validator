73a0d41df9f6b2db925560d0c2b9d300da89e96d
Fang Han <fhan@vmware.com>
2022-01-04 16:38:35 -0800

    Add run.sh wrapper for Sonobuoy.

--- run.sh
+++ run.sh
@@ -10,16 +10,11 @@ results_dir="${RESULTS_DIR:-/tmp/results}"
 # saveResults prepares the results for handoff to the Sonobuoy worker.
 # See: https://github.com/vmware-tanzu/sonobuoy/blob/master/docs/plugins.md
 saveResults() {
-  cd "${results_dir}"
-
-  # Sonobuoy worker expects a tar file.
-	tar czf results.tar.gz out.json
-
 	# Signal to the worker that we are done and where to find the results.
-	printf "${results_dir}"/results.tar.gz > "${results_dir}"/done
+	printf "${results_dir}"/out.json > "${results_dir}"/done
 }
 
 # Ensure that we tell the Sonobuoy worker we are done regardless of results.
 trap saveResults EXIT
 
-./svc-test > "${results_dir}"/out.json
+./svc-test | tee "${results_dir}"/out.json
