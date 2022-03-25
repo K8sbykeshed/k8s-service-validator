d18dbb352d5cf5e621b8a450ac9915adf2242620
Zac Yang <yzac00@gmail.com>
2022-03-25 16:18:48 -0700

    refined metallb.yaml

--- hack/install_metallb.sh
+++ hack/install_metallb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl apply -f ./hack/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 cat <<EOF | kubectl apply -f -
